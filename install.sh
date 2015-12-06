fb99bc9492b51fc11cbda910beeb03a3156877ad
Ben Lampert <lampert.b@gmail.com>
2015-12-05 19:52:18 -0800

    Updated install.sh to add python-dev

--- install.sh
+++ install.sh
@@ -5,23 +5,31 @@ echo "Starting install script, please remember to launch as sudo."
 
 #Add the boot script to the init.d
 #This will make the AFIB software launch on reboot
+echo "Creating a boot script."
 cp ./Bootscript/AFIBBoot /etc/init.d/AFIBBoot
 chmod 755 /etc/init.d/AFIBBoot
 update-rc.d AFIBBoot defaults
+echo "Done"
 
 #Next install relevant packages
+echo "Installing wiring Pi."
 apt-get update
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 ./build
 cd ..
+echo "Done"
 
 #Next install the python libraries needed
+echo "Installing neccessary packages"
+apt-get install python-dev
 apt-get install python-numpy python-scipy
+echo "Done"
 
 #Next install the AFIB software
+echo "Building software package"
 cd ./Software
 make clean
 make
 cd ..
-
+echo "Done"
